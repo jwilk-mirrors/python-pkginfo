7074d9734143f8e70c32ae87c39bd42f3d294fdf
Tres Seaver <tseaver@palladion.com>
2023-01-06 10:35:13 -0500

    Fix typing errors / gaps reported from  CI failure.  LP #2002104.

--- pkginfo/distribution.pyi
+++ pkginfo/distribution.pyi
@@ -1,11 +1,15 @@
+import email.message
 from typing import Callable
+from typing import Dict
 from typing import Generator
+from typing import List
 from typing import Sequence
+from typing import TextIO
 from typing import Tuple
 
-def parse(fp): ...
-def get(msg, header): ...
-def get_all(msg, header): ...
+def parse(fp: TextIO) -> email.message.Message: ...
+def get(msg: email.message.Message, header: str) -> str: ...
+def get_all(msg: email.message.Message, header: str) -> List[str]: ...
 
 _header_attr_triple = Tuple[str, str, bool]
 _header_attrs = Tuple[_header_attr_triple]
@@ -15,10 +19,10 @@ HEADER_ATTRS_1_2: _header_attrs
 HEADER_ATTRS_2_0 = HEADER_ATTRS_1_2
 HEADER_ATTRS_2_1: _header_attrs
 HEADER_ATTRS_2_2: _header_attrs
-HEADER_ATTRS: _header_attrs
+HEADER_ATTRS: Dict[str, _header_attrs]
 
 class Distribution:
-    metadata_version: str
+    metadata_version: str | None
     name: str
     version: str
     platforms: Sequence[str]
@@ -47,7 +51,7 @@ class Distribution:
     description_content_type: str
     dynamic: Sequence[str]
     def extractMetadata(self) -> None: ...
-    def read(self) -> None: ...
-    def parse(self, data) -> None: ...
+    def read(self) -> bytes: ...
+    def parse(self, data: bytes) -> None: ...
     def __iter__(self) -> Generator[str, None, None]: ...
     iterkeys: Generator[str, None, None]
