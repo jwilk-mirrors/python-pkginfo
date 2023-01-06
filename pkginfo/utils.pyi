98cd42cd3e58de543884dd6737e1c2ef364ea27e
Tres Seaver <tseaver@palladion.com>
2023-01-06 11:02:26 -0500

    Add stricter mytype checks and fix
    Matching the checks used in 'twine'.

--- pkginfo/utils.pyi
+++ pkginfo/utils.pyi
@@ -1,9 +1,5 @@
 import types
 
-from .bdist import BDist as BDist
-from .develop import Develop as Develop
-from .installed import Installed as Installed
-from .sdist import SDist as SDist
-from .wheel import Wheel as Wheel
+from .distribution import Distribution
 
-def get_metadata(path_or_module: str | types.ModuleType, metadata_version: str | None = ...): ...
+def get_metadata(path_or_module: str | types.ModuleType, metadata_version: str | None = ...) -> Distribution | None: ...
